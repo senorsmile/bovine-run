ea48b08e98a851e8d1061e5068c5cce4d8232b8a
Shaun Smiley <senorsmile@gmail.com>
2017-08-04 15:56:42 -0700

    fix callback_name

--- callback_plugins/bovine_json.py
+++ callback_plugins/bovine_json.py
@@ -26,7 +26,7 @@ default_json=False
 class CallbackModule(CallbackBase):
     CALLBACK_VERSION = 2.0
     CALLBACK_TYPE = 'notification'
-    CALLBACK_NAME = 'json2'
+    CALLBACK_NAME = 'bovine_json'
     CALLBACK_NEEDS_WHITELIST = True
 
     def __init__(self, display=None):
